c12cef3adb7cbf004fdcd5aab9f5375062015c37
Anastasiya Soloviova <mafanya.as@mail.ru>
2025-07-20 14:41:51 +0300

    add info for users app

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from users.apps import UsersConfig
 from django.contrib.auth.views import LoginView, LogoutView
-from users.views import RegisterView
+from users.views import RegisterView, UserDetailView, UserUpdateView
 
 
 app_name = UsersConfig
@@ -10,5 +10,7 @@ app_name = UsersConfig
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', LoginView.as_view(template_name='users/login.html'), name='login'),
-    path('logout/', LogoutView.as_view(next_page='mailings:mailing_list'), name='logout')
+    path('logout/', LogoutView.as_view(next_page='mailings:mailing_list'), name='logout'),
+    path('user/<int:pk>', UserDetailView.as_view(), name='user'),
+    path('user_update/<int:pk>', UserUpdateView.as_view(), name='user_update'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,4 +1,3 @@
-from django.contrib.auth.forms import UserChangeForm
 from django.views.generic import CreateView, DetailView, UpdateView
 from users.forms import UserRegisterForm
 from django.core.mail import send_mail
@@ -7,18 +6,23 @@ from users.models import User
 
 
 class RegisterView(CreateView):
+    """Представление для регистрации нового пользователя.
+    После успешной регистрации отправляет приветственное письмо."""
     template_name = 'users/register.html'
     form_class = UserRegisterForm
     success_url = reverse_lazy('users:login')
 
 
     def form_valid(self, form):
+        """Обрабатывает валидную форму регистрации.
+        Сохраняет пользователя и отправляет приветственное письмо."""
         user = form.save()
         self.send_welcome_email(user.email)
         return super().form_valid(form)
 
 
     def send_welcome_email(self, user_email):
+        """Отправляет приветственное письмо на указанный email."""
         subject = 'Добро пожаловать на сайт!'
         message = 'Спасибо за выбор нашего сайта!'
         from_email = 'asta.soul@yandex.ru'
@@ -27,21 +31,27 @@ class RegisterView(CreateView):
 
 
 class UserDetailView(DetailView):
+    """Представление для отображения информации о текущем пользователе.
+    Использует текущего авторизованного пользователя."""
     model = User
     template_name = 'users/user.html'
     context_object_name = 'user'
 
 
     def get_object(self):
+        """Возвращает текущего авторизованного пользователя."""
         return self.request.user
 
 
 class UserUpdateView(UpdateView):
+    """Представление для редактирования данных текущего пользователя.
+    После обновления перенаправляет на страницу профиля."""
     model = User
-    form_class = UserChangeForm
+    form_class = UserRegisterForm
     template_name = 'users/user_update.html'
     success_url = reverse_lazy('users:user')
 
 
     def get_object(self):
+        """Возвращает текущего авторизованного пользователя для редактирования."""
         return self.request.user
