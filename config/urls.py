20ab36d75e37d956b897b4241854a77146461440
Anastasiya Soloviova <mafanya.as@mail.ru>
2025-07-20 12:35:51 +0300

    add info for users app

--- config/urls.py
+++ config/urls.py
@@ -6,6 +6,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('mailings.apps', namespace='mailings')),
+    path('', include('mailings.urls', namespace='mailings')),
     path('users/', include('users.urls', namespace='users')),
 ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
 from users.apps import UsersConfig
+from django.contrib.auth.views import LoginView, LogoutView
+from users.views import RegisterView
+
 
 app_name = UsersConfig
 
-urlpatterns = [
-    path(''),
 
+urlpatterns = [
+    path('register/', RegisterView.as_view(), name='register'),
+    path('login/', LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(next_page='mailings:mailing_list'), name='logout')
 ]
