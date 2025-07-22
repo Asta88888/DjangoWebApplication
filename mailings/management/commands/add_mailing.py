95502422c9cafe9f447e33ea265cc664712e20cc
Anastasiya Soloviova <mafanya.as@mail.ru>
2025-07-22 10:11:40 +0300

    add command for create message and mailing to db

---
+++ mailings/management/commands/add_mailing.py
@@ -0,0 +1,35 @@
+from django.core.management.base import BaseCommand
+from mailings.models import Mailing, Message
+
+
+class Command(BaseCommand):
+    help = "Добавление рассылки и сообщений в базу данных"
+
+
+    def handle(self, *args, **options):
+        messages_data = [
+            {'topic': 'Камчатка', 'content': 'Камчатка стоит посещения благодаря своей уникальной природе: активным вулканам, гейзерам, термальным источникам и дикому животному миру. Это место, где можно увидеть захватывающие пейзажи, от горных вершин до морских побережий, и испытать незабываемые приключения, такие как восхождение на вулканы, рыбалка или сплав по рекам.'},
+            {'topic': 'Карелия', 'content': 'Карелия стоит посещения из-за своей уникальной природы: живописные озера, реки, леса и горы, а также исторические и культурные достопримечательности, такие как остров Кижи и горный парк Рускеала.'},
+            {'topic': 'Крым', 'content': 'Крым стоит посетить из-за его уникального сочетания природных красот, богатой истории и культурного разнообразия. Полуостров предлагает разнообразные ландшафты: от гор и степей до субтропических лесов и живописного побережья. Здесь можно найти как тихие уединенные места, так и курорты с развитой инфраструктурой. Кроме того, Крым славится своими целебными климатом, историческими достопримечательностями и вкусной местной кухней.'},
+        ]
+        messages = {}
+        for data in messages_data:
+            message, created = Message.objects.get_or_create(**data)
+            messages[message.topic] = message
+            if created:
+                self.stdout.write(self.style.SUCCESS(f'Создано сообщение: {message.topic}'))
+            else:
+                self.stdout.write(self.style.WARNING(f'Сообщение {message.topic} уже существует'))
+
+        mailings = [
+            {'name': 'Теплый климат', 'message': messages['Крым']},
+            {'name': 'Холодный климат', 'message': messages['Карелия']},
+            {'name': 'Необычные места', 'message': messages['Камчатка']},
+        ]
+
+        for mailing_data in mailings:
+            mailing, created = Mailing.objects.get_or_create(**mailing_data)
+            if created:
+                self.stdout.write(self.style.SUCCESS(f'Создана рассылка: {mailing.name}'))
+            else:
+                self.stdout.write(self.style.WARNING(f'Рассылка {mailing.name} уже существует'))
--- mailings/urls.py
+++ mailings/urls.py
@@ -1,9 +1,24 @@
 from django.urls import path
 from mailings.apps import MailingsConfig
+from mailings.views import MailingListView, MailingDetailView, MailingCreateView, MailingUpdateView, MailingDeleteView, MessageListView, MessageDetailView, MessageCreateView, MessageUpdateView, MessageDeleteView, RecipientListView, RecipientDetailView, RecipientCreateView, RecipientUpdateView, RecipientDeleteView
 
 app_name = MailingsConfig
 
 urlpatterns = [
-    path(''),
+    path('mailings/mailing_list', MailingListView.as_view(), name='mailing_list'),
+    path('mailings/<int:pk>', MailingDetailView.as_view(), name='mailing_detail'),
+    path('mailings/create', MailingCreateView.as_view(), name='mailing_create'),
+    path('mailings/<int:pk>/update', MailingUpdateView.as_view(), name='mailing_update'),
+    path('mailings/<int:pk>/delete', MailingDeleteView.as_view(), name='mailing_delete'),
+    path('mailings/message_list', MessageListView.as_view(), name='message_list'),
+    path('mailings/message/<int:pk>', MessageDetailView.as_view(), name='message_detail'),
+    path('mailings/message/create', MessageCreateView.as_view(), name='message_create'),
+    path('mailings/message/<int:pk>/update', MessageUpdateView.as_view(), name='message_update'),
+    path('mailings/message/<int:pk>/delete', MessageDeleteView.as_view(), name='message_delete'),
+    path('mailings/recipient_list', RecipientListView.as_view(), name='recipient_list'),
+    path('mailings/recipient/<int:pk>', RecipientDetailView.as_view(), name='recipient_detail'),
+    path('mailings/recipient/create', RecipientCreateView.as_view(), name='recipient_create'),
+    path('mailings/recipient/<int:pk>/update', RecipientUpdateView.as_view(), name='recipient_update'),
+    path('mailings/recipient/<int:pk>/delete', RecipientDeleteView.as_view(), name='recipient_delete'),
 
 ]
