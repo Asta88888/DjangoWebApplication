5176a2355cf9b33930575f51486858318195761a
Anastasiya Soloviova <mafanya.as@mail.ru>
2025-07-24 12:33:40 +0300

    fixed mistake in urls

--- mailings/urls.py
+++ mailings/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 from mailings.apps import MailingsConfig
 from mailings.views import MailingListView, MailingDetailView, MailingCreateView, MailingUpdateView, MailingDeleteView, MessageListView, MessageDetailView, MessageCreateView, MessageUpdateView, MessageDeleteView, RecipientListView, RecipientDetailView, RecipientCreateView, RecipientUpdateView, RecipientDeleteView
 
-app_name = MailingsConfig
+app_name = MailingsConfig.name
 
 urlpatterns = [
-    path('mailings/mailing_list', MailingListView.as_view(), name='mailing_list'),
+    path('', MailingListView.as_view(), name='mailing_list'),
     path('mailings/<int:pk>', MailingDetailView.as_view(), name='mailing_detail'),
     path('mailings/create', MailingCreateView.as_view(), name='mailing_create'),
     path('mailings/<int:pk>/update', MailingUpdateView.as_view(), name='mailing_update'),
