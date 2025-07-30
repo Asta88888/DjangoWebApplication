402f296f38c40d7d468b0a22dba07d49df46ed47
Anastasiya Soloviova <mafanya.as@mail.ru>
2025-07-30 15:02:07 +0300

    add perms

--- mailings/views.py
+++ mailings/views.py
@@ -1,4 +1,5 @@
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
+from django.core.exceptions import PermissionDenied
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.urls import reverse_lazy
@@ -7,21 +8,37 @@ from mailings.models import Mailing, Message, Recipient
 from mailings.forms import MailingForm, RecipientForm, MessageForm
 
 
-class MailingListView(LoginRequiredMixin, ListView):
+class MailingListView(LoginRequiredMixin, PermissionRequiredMixin, ListView):
     """Представление для отображения списка всех рассылок."""
     model = Mailing
+    permission_required = 'mailings.list_mailing'
 
 
-class MailingDetailView(LoginRequiredMixin, DetailView):
+    def get_queryset(self):
+        if self.request.user.has_perm('mailings.list_mailing'):
+            return Mailing.objects.all()
+        return Mailing.objects.filter(owner=self.request.user)
+
+
+class MailingDetailView(LoginRequiredMixin, PermissionRequiredMixin, DetailView):
     """Представление для отображения деталей конкретной рассылки."""
     model = Mailing
+    permission_required = 'mailings.detail_mailing'
 
 
-class MailingCreateView(LoginRequiredMixin, CreateView):
+    def get_object(self, queryset=None):
+        obj = super().get_object(queryset)
+        if not (obj.owner == self.request.user or self.request.user.has_perm('mailings.detail_mailing')):
+            raise PermissionDenied
+        return obj
+
+
+class MailingCreateView(LoginRequiredMixin, PermissionRequiredMixin, CreateView):
     """Представление для создания новой рассылки."""
     model = Mailing
     form_class = MailingForm
     success_url = reverse_lazy('mailings:mailing_list')
+    permission_required = 'mailings.create_mailing'
     
     
     def form_valid(self, form):
@@ -32,34 +49,65 @@ class MailingCreateView(LoginRequiredMixin, CreateView):
         return super().form_valid(form)
 
 
-class MailingUpdateView(LoginRequiredMixin, UpdateView):
+class MailingUpdateView(LoginRequiredMixin, PermissionRequiredMixin, UpdateView):
     """Представление для редактирования существующей рассылки."""
     model = Mailing
     form_class = MailingForm
     success_url = reverse_lazy('mailings:mailing_list')
+    permission_required = 'mailings.change_mailing'
 
 
-class MailingDeleteView(LoginRequiredMixin, DeleteView):
+    def get_object(self, queryset=None):
+        obj = super().get_object(queryset)
+        if obj.owner != self.request.user:
+            raise PermissionDenied
+        return obj
+
+
+class MailingDeleteView(LoginRequiredMixin, PermissionRequiredMixin, DeleteView):
     """Представление для удаления рассылки."""
     model = Mailing
     success_url = reverse_lazy('mailings:mailing_list')
+    permission_required = 'mailings.delete_mailing'
+
+    def get_object(self, queryset=None):
+        obj = super().get_object(queryset)
+        if obj.owner != self.request.user:
+            raise PermissionDenied
+        return obj
 
 
-class RecipientListView(LoginRequiredMixin, ListView):
+class RecipientListView(LoginRequiredMixin, PermissionRequiredMixin, ListView):
     """Представление для отображения списка всех получателей."""
     model = Recipient
+    permission_required = 'mailings.list_recipient'
 
 
-class RecipientDetailView(LoginRequiredMixin, DetailView):
+    def get_queryset(self):
+        if self.request.user.has_perm('mailings.list_recipient'):
+            return Recipient.objects.all()
+        return Recipient.objects.filter(owner=self.request.user)
+
+
+class RecipientDetailView(LoginRequiredMixin, PermissionRequiredMixin, DetailView):
     """Представление для отображения деталей конкретного получателя."""
     model = Recipient
+    permission_required = 'mailings.detail_recipient'
 
 
-class RecipientCreateView(LoginRequiredMixin, CreateView):
+    def get_object(self, queryset=None):
+        obj = super().get_object(queryset)
+        if not (obj.owner == self.request.user or self.request.user.has_perm('mailings.detail_recipient')):
+            raise PermissionDenied
+        return obj
+
+
+class RecipientCreateView(LoginRequiredMixin, PermissionRequiredMixin, CreateView):
     """Представление для добавления нового получателя."""
     model = Recipient
     form_class = RecipientForm
     success_url = reverse_lazy('mailings:recipient_list')
+    permission_required = 'mailings.create_recipient'
 
 
     def form_valid(self, form):
@@ -70,30 +118,60 @@ class RecipientCreateView(LoginRequiredMixin, CreateView):
         return super().form_valid(form)
 
 
-class RecipientUpdateView(LoginRequiredMixin, UpdateView):
+class RecipientUpdateView(LoginRequiredMixin, PermissionRequiredMixin, UpdateView):
     """Представление для редактирования информации о получателе."""
     model = Recipient
     form_class = RecipientForm
     success_url = reverse_lazy('mailings:recipient_list')
+    permission_required = 'mailings.change_recipient'
+
+    def get_object(self, queryset=None):
+        obj = super().get_object(queryset)
+        if obj.owner != self.request.user:
+            raise PermissionDenied
+        return obj
 
 
-class RecipientDeleteView(LoginRequiredMixin, DeleteView):
+class RecipientDeleteView(LoginRequiredMixin, PermissionRequiredMixin, DeleteView):
     """Представление для удаления получателя."""
     model = Recipient
     success_url = reverse_lazy('mailings:recipient_list')
+    permission_required = 'mailings.delete_recipient'
+
+    def get_object(self, queryset=None):
+        obj = super().get_object(queryset)
+        if obj.owner != self.request.user:
+            raise PermissionDenied
+        return obj
 
 
-class MessageListView(LoginRequiredMixin, ListView):
+class MessageListView(LoginRequiredMixin, PermissionRequiredMixin, ListView):
     """Представление для отображения списка сообщений."""
     model = Message
+    permission_required = 'mailings.list_message'
 
 
-class MessageDetailView(LoginRequiredMixin, DetailView):
+    def get_queryset(self):
+        if self.request.user.has_perm('mailings.list_message'):
+            return Message.objects.all()
+        return Message.objects.filter(owner=self.request.user)
+
+
+
+class MessageDetailView(LoginRequiredMixin, PermissionRequiredMixin, DetailView):
     """Представление для просмотра деталей сообщения."""
     model = Message
+    permission_required = 'mailings.detail_message'
 
 
-class MessageCreateView(LoginRequiredMixin, CreateView):
+    def get_object(self, queryset=None):
+        obj = super().get_object(queryset)
+        if not (obj.owner == self.request.user or self.request.user.has_perm('mailings.detail_message')):
+            raise PermissionDenied
+        return obj
+
+
+class MessageCreateView(LoginRequiredMixin, PermissionRequiredMixin, CreateView):
     """Представление для создания нового сообщения."""
     model = Message
     form_class = MessageForm
@@ -108,17 +186,31 @@ class MessageCreateView(LoginRequiredMixin, CreateView):
         return super().form_valid(form)
 
 
-class MessageUpdateView(LoginRequiredMixin, UpdateView):
+class MessageUpdateView(LoginRequiredMixin, PermissionRequiredMixin, UpdateView):
     """Представление для редактирования сообщения."""
     model = Message
     form_class = MessageForm
     success_url = reverse_lazy('mailings:message_list')
+    permission_required = 'mailings.change_message'
+
+    def get_object(self, queryset=None):
+        obj = super().get_object(queryset)
+        if obj.owner != self.request.user:
+            raise PermissionDenied
+        return obj
 
 
-class MessageDeleteView(LoginRequiredMixin, DeleteView):
+class MessageDeleteView(LoginRequiredMixin, PermissionRequiredMixin, DeleteView):
     """Представление для удаления сообщения."""
     model = Message
     success_url = reverse_lazy('mailings:message_list')
+    permission_required = 'mailings.delete_message'
+
+    def get_object(self, queryset=None):
+        obj = super().get_object(queryset)
+        if obj.owner != self.request.user:
+            raise PermissionDenied
+        return obj
 
 
 def main_page(request):
